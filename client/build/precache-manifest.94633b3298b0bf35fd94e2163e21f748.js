2cde2c75bc98ff9e5ab101e360337af486ef24f2
Christie Lynam <christielynam@gmail.com>
2019-07-14 19:22:58 -0600

    unignore build

---
+++ client/build/precache-manifest.94633b3298b0bf35fd94e2163e21f748.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f78546a282e0823f14753663826945e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e8d5f12bd1cb4216d0b",
+    "url": "/static/css/main.3fee09b9.chunk.css"
+  },
+  {
+    "revision": "859b1facdc9cdf1432e7",
+    "url": "/static/js/2.413ef12a.chunk.js"
+  },
+  {
+    "revision": "3e8d5f12bd1cb4216d0b",
+    "url": "/static/js/main.56617d0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a284d700c4068a27dc477cbdc01b0095",
+    "url": "/static/media/no-back.a284d700.png"
+  },
+  {
+    "revision": "2b344a1a696c5f4fb657a58802b7961b",
+    "url": "/static/media/wordcloud_man.2b344a1a.png"
+  }
+]);
