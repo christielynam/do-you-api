6964d96b69f42ed6ec9c408e8c25a3d86d207d4a
Christie Lynam <christielynam@gmail.com>
2019-07-19 18:53:52 -0600

    Refactor App to use implicit return

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.scss';
+import React from 'react'
+import './App.scss'
 import { Route, Switch } from 'react-router-dom'
 import LandingPage from '../../containers/LandingPage'
 import Header from '../../containers/Header'
@@ -9,20 +9,19 @@ import Controls from '../../containers/Controls'
 // import Profile from '../../containers/Profile'
 import PersonalityTypes from '../../containers/PersonalityTypes'
 
-const App = () => {
-  return (
-    <div className='App'>
-      <Switch>
-        <Route exact path='/' component={LandingPage} />
-        <Route path='/' component={Header} />
-      </Switch>
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/signup' component={Signup} />
-      <Route exact path='/controls' component={Controls} />
-      {/* {/* <Route exact path='/profile' component={Profile} /> */}
-      <Route exact path='/personality-types' component={PersonalityTypes} />
-    </div>
-  )
-}
+const App = () => (
+  <div className='App'>
+    <Switch>
+      <Route exact path='/' component={LandingPage} />
+      <Route path='/' component={Header} />
+    </Switch>
+    <Route exact path='/login' component={Login} />
+    <Route exact path='/signup' component={Signup} />
+    <Route exact path='/controls' component={Controls} />
+    {/* {/* <Route exact path='/profile' component={Profile} /> */}
+    <Route exact path='/personality-types' component={PersonalityTypes} />
+  </div>
+)
+
 
 export default App
