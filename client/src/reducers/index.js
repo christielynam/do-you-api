826650730f8cc766f4a617a580036bca2c3a9cb5
Christie Lynam <christielynam@gmail.com>
2019-07-19 18:18:28 -0600

    Update root reducerf

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,16 +1,20 @@
 import { combineReducers } from 'redux'
-import { user } from './user-reducer'
-import { loading } from './loading-reducer'
-import { error } from './error-reducer'
 import { assessments } from './assessments-reducer'
+import { error } from './error-reducer'
+import { loading } from './loading-reducer'
 import { personalities } from './personalities-reducer'
+import { results } from './results-reducer'
+import { slides } from './slides-reducer'
 import { tests } from './tests-reducer'
+import { user } from './user-reducer'
 
 export const rootReducer = combineReducers({
   assessments,
   error,
   loading,
   personalities,
+  results,
+  slides,
   tests,
   user
 })
